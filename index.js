a2161249bff9db9a05b61ef8e978ddb133775642
deveHk <connect.to.hk@gmail.com>
2023-01-20 01:44:03 +0530

    default

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 3001;
+
 app.use(cors());
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
